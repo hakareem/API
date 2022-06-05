4bcf364e34aaa85554001c7fe2f8b46b2d505b24
hakareem <harithakareem72@gmail.com>
2022-06-05 22:08:14 +0100

    text

--- static/main.js
+++ static/main.js
@@ -7,8 +7,9 @@ async function apiRequest() {
 
   console.log(data);
   document.querySelector("img").src = data.img;
-  document.querySelector("#founded").textContent = data.founded;
-  document.querySelector("#nickname").textContent = data.nickname;
-  document.querySelector("#manager").textContent = data.manager;
-  document.querySelector("#league").textContent = data.league;
+  document.querySelector("#founded").textContent = "Founded: " + data.founded;
+  document.querySelector("#nickname").textContent =
+    "Nickname: " + data.nickname;
+  document.querySelector("#manager").textContent = "Manager: " + data.manager;
+  document.querySelector("#league").textContent = "League: " + data.league;
 }
